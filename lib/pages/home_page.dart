8e227453e470eef4f0848a28ba18aea488b707e8
Your Name <chamidilshan55@gmail.com>
2023-01-21 17:16:36 +0530

    added search bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,19 +7,51 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.all(40),
         child: ListView(
           children: [
             Container(
-              height: 50.0,
-              width: 50.0,
+              height: 150.0,
+              width: 300.0,
               decoration: BoxDecoration(
-                color: Colors.purple
+                color: Colors.purple,
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                      'Select a programming\n language and explore courses',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white54,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        hintText: 'Find a languague',
+                        prefixIcon: Icon(Icons.search),
+                        prefixIconColor: Colors.grey
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-            Text(
-              'Testing'
-            )
           ],
         ),
       ),
