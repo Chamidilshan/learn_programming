ce50f11f1cf47e7a1eff795c611721cf455439a4
Your Name <chamidilshan55@gmail.com>
2023-01-24 21:32:34 +0530

    edited C language page

--- lib/pages/language_pages/cLang.dart
+++ lib/pages/language_pages/cLang.dart
@@ -6,9 +6,15 @@ class CPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [Container(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          decoration: BoxDecoration(
+              color: Color(0xFF35323d),
+            borderRadius: BorderRadius.circular(40)
+          ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('C'),
               SizedBox(
@@ -18,13 +24,19 @@ class CPage extends StatelessWidget {
                 'C is a general-purpose, high-level programming language that was '
                     'developed in the early 1970s by Dennis Ritchie at '
                     'Bell Labs. It is considered a "mid-level" '
-                    'language, as it has features of both high-level and low-level languages.'
+                    'language, as it has features of both high-level and low-level languages.',
+              ),
+              SizedBox(
+                height: 20.0,
               ),
               Text('C is widely used for system programming, including operating systems, '
                   'embedded systems, and device drivers, as well as for application '
                   'development in various fields such as video games, financial and '
                   'trading systems, simulation '
                   'and modeling, and scientific computing.'),
+              SizedBox(
+                height: 20.0,
+              ),
               Text('One of the main strengths of C is its ability to provide direct access '
                   'to memory and low-level system resources, making it a highly '
                   'efficient and powerful language. It also has a relatively '
@@ -32,7 +44,6 @@ class CPage extends StatelessWidget {
             ],
           ),
         ),
-        ],
       ),
     );
   }
