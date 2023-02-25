6ee72f22d50923c263bc3cf8955507db4beff8cc
Your Name <chamidilshan55@gmail.com>
2023-02-25 20:20:10 +0530

    edited language page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learn_programming/pages/home_page.dart';
+import 'package:learn_programming/pages/language_pages/cLang.dart';
+import 'package:learn_programming/widgets/language_button.dart';
 import 'pages/started_page.dart';
 
 void main() {
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: CPage(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/pages/language_pages/cLang.dart
+++ lib/pages/language_pages/cLang.dart
@@ -6,43 +6,58 @@ class CPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          decoration: BoxDecoration(
-              color: Colors.red,
-            borderRadius: BorderRadius.circular(40)
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('C'),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                'C is a general-purpose, high-level programming language that was '
-                    'developed in the early 1970s by Dennis Ritchie at '
-                    'Bell Labs. It is considered a "mid-level" '
-                    'language, as it has features of both high-level and low-level languages.',
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text('C is widely used for system programming, including operating systems, '
-                  'embedded systems, and device drivers, as well as for application '
-                  'development in various fields such as video games, financial and '
-                  'trading systems, simulation '
-                  'and modeling, and scientific computing.'),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text('One of the main strengths of C is its ability to provide direct access '
-                  'to memory and low-level system resources, making it a highly '
-                  'efficient and powerful language. It also has a relatively '
-                  'small and simple syntax, making it easy to learn and use.'),
-            ],
-          ),
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              padding: EdgeInsets.all(40.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'C',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      'C is a general-purpose, high-level programming language that was '
+                          'developed in the early 1970s by Dennis Ritchie at '
+                          'Bell Labs. It is considered a "mid-level" '
+                          'language, as it has features of both high-level and low-level languages.',
+                    ),
+                  ],
+                ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+
+            // SizedBox(
+            //   height: 20.0,
+            // ),
+            // Text('C is widely used for system programming, including operating systems, '
+            //     'embedded systems, and device drivers, as well as for application '
+            //     'development in various fields such as video games, financial and '
+            //     'trading systems, simulation '
+            //     'and modeling, and scientific computing.'),
+            // SizedBox(
+            //   height: 20.0,
+            // ),
+            // Text('One of the main strengths of C is its ability to provide direct access '
+            //     'to memory and low-level system resources, making it a highly '
+            //     'efficient and powerful language. It also has a relatively '
+            //     'small and simple syntax, making it easy to learn and use.'),
+          ],
         ),
       ),
     );
