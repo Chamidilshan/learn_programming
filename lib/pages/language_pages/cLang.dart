f6ef16a454f5630a811f3a0e90305c5bec782f97
Your Name <chamidilshan55@gmail.com>
2023-02-26 15:18:34 +0530

    added courses list

--- lib/pages/language_pages/cLang.dart
+++ lib/pages/language_pages/cLang.dart
@@ -11,59 +11,71 @@ class CPage extends StatelessWidget {
         backgroundColor: Color(0XFFfeebcd),
       ),
       body: SingleChildScrollView(
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(
-                  height: 20.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(40.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0XFFfcac63),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    padding: EdgeInsets.all(40.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'C',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20.0,
-                          ),
-                          Text(
-                            'C is a general-purpose, high-level programming language that was '
-                                'developed in the early 1970s by Dennis Ritchie at '
-                                'Bell Labs. It is considered a "mid-level" '
-                                'language, as it has features of both high-level and low-level languages.',
-                          ),
-                        ],
-                      ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 40.0),
-                  child: Text(
-                      'Courses',
-                    style: TextStyle(
-                      fontSize: 20.0
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-              ],
+            SizedBox(
+              height: 20.0,
             ),
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0XFFfcac63),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                padding: EdgeInsets.all(40.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'C',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text(
+                        'C is a general-purpose, high-level programming language that was '
+                            'developed in the early 1970s by Dennis Ritchie at '
+                            'Bell Labs. It is considered a "mid-level" '
+                            'language, as it has features of both high-level and low-level languages.',
+                      ),
+                    ],
+                  ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 40.0),
+              child: Text(
+                  'Courses',
+                style: TextStyle(
+                  fontSize: 20.0
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Color(0xFF124f50),
+                  ),
+                  padding: EdgeInsets.symmetric(),
+                  height: 50,
+                  child: const Center(child: Text('Course Name')),
+                );
+              },
+              itemCount: 5,
+            )
           ],
         ),
       ),
