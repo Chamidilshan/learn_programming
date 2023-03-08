ec2b0edf94327d4054ba5904e853c89974440f97
Your Name <chamidilshan55@gmail.com>
2023-03-08 17:37:58 +0530

    added styles to list view

--- lib/pages/course_page.dart
+++ lib/pages/course_page.dart
@@ -11,34 +11,46 @@ class _CoursesPageState extends State<CoursesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor:Color(0xFFFedb92f),
+      backgroundColor: const Color(0xFF35323d),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               child: Padding(
-                padding: const EdgeInsets.only(left: 40.0, right: 40.0,),
+                padding: const EdgeInsets.only(left: 40.0, right: 40.0, top: 40.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('C Coureses',
-                        style: TextStyle(color: Colors.black,
-                          fontSize: 28.0,
-                          fontWeight: FontWeight.bold
-                        ),
-                        ),
-                        Image.asset('images/assC.png', width: 120.0,)
-                      ],
+                    Text('C',
+                    style: TextStyle(color: Colors.white,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold
                     ),
-                    // Image.asset('images/books.png', width: 80.0,),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text('Programming',
+                      style: TextStyle(color: Colors.white,
+                          fontSize: 14.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text('Languague',
+                      style: TextStyle(color: Colors.white,
+                          fontSize: 14.0,
+                      ),
+                    ),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         SizedBox(
                           width: 50.0,
                         ),
+                        Image.asset('images/assC.png', width: 180.0,),
                         // Container(
                         //   decoration: BoxDecoration(
                         //     borderRadius: BorderRadius.circular(10.0),
@@ -51,90 +63,97 @@ class _CoursesPageState extends State<CoursesPage> {
                   ],
                 ),
               ),
-              height: 200.0,
+              height: 320.0,
               width: double.infinity,
               decoration: BoxDecoration(
               ),
             ),
-            ClipRRect(
-              child: Container(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 20.0, left: 20.0),
-                  child: Text('Explore Courses',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w900
-                    ),
-                  ),
-                ),
-              ),
-            ),
             Container(
-              padding: EdgeInsets.all(40.0),
-              child: ListView.separated(itemCount: 10,shrinkWrap: true, itemBuilder: (context, index){
-                return GestureDetector(
-                  onTap: () {print('Pressed');},
-                  child: Container(
-                    child: Row(
-                      children: [
-                        SizedBox(width: 10.0),
-                        Image.asset('images/code.png', width: 40.0,),
-                        SizedBox(
-                          width: 10.0,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40.0), topRight: Radius.circular(40.0)
+                  )
+              ),
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                   Text('Explore Courses',
+                     style: TextStyle(
+                         fontSize: 20.0,
+                         fontWeight: FontWeight.w900
+                     ),
+                   ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ListView.separated(itemCount: 10,shrinkWrap: true, itemBuilder: (context, index){
+                    return GestureDetector(
+                      onTap: () {print('Pressed');},
+                      child: Container(
+                        child: Row(
                           children: [
-                            SizedBox(height: 10.0),
-                            Text('Introduction to C Programming',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600
-                            )
+                            SizedBox(width: 10.0),
+                            Image.asset('images/code.png', width: 40.0,),
+                            SizedBox(
+                              width: 10.0,
                             ),
-                            SizedBox(height: 10.0),
-                            Row(
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text('Udemy',
-                                    style: TextStyle(
-                                      color: Colors.grey.shade700
-                                    ),
-                                  ),
-                                SizedBox(
-                                  width: 20.0,
+                                SizedBox(height: 10.0),
+                                Text('Introduction to C Programming',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600
+                                )
                                 ),
-                                Text('6 Hours',
-                                    style: TextStyle(
-                                        color: Colors.grey.shade700
+                                SizedBox(height: 10.0),
+                                Row(
+                                  children: [
+                                    Text('Udemy',
+                                        style: TextStyle(
+                                          color: Colors.grey.shade700
+                                        ),
+                                      ),
+                                    SizedBox(
+                                      width: 20.0,
                                     ),
+                                    Text('6 Hours',
+                                        style: TextStyle(
+                                            color: Colors.grey.shade700
+                                        ),
+                                    )
+                                  ],
                                 )
                               ],
-                            )
+                            ),
+                            SizedBox(
+                              width: 60.0,
+                            ),
+                            Image.asset('images/award.png', width: 36.0,),
                           ],
                         ),
-                        SizedBox(
-                          width: 60.0,
+                        width: double.infinity,
+                        height: 70.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFF1D1617).withOpacity(0.07),
+                              blurRadius: 40.0,
+                              spreadRadius: 0
+                            )
+                          ]
                         ),
-                        Image.asset('images/award.png', width: 36.0,),
-                      ],
-                    ),
-                    width: double.infinity,
-                    height: 70.0,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color(0xFF1D1617).withOpacity(0.07),
-                          blurRadius: 40.0,
-                          spreadRadius: 0
-                        )
-                      ]
-                    ),
+                      ),
+                    );
+                  }, separatorBuilder: ( context,  index) => SizedBox(
+                    height: 20.0,
                   ),
-                );
-              }, separatorBuilder: ( context,  index) => SizedBox(
-                height: 20.0,
-              ),
+                  ),
+                ],
               ),
             )
           ],
@@ -162,3 +181,5 @@ class CustomClipPath extends CustomClipper<Path>{
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
     return false;
   }}
+
+
